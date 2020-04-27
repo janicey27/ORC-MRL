659920c96efcf8833aa3d94190dfbafb918a2e73
Amine <amine_bennouna@hotmail.com>
2020-04-27 01:06:34 -0400

    Added some testing there, at this state it plots accuracy by number of samples

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ Created on Sun Mar  1 18:51:20 2020
 from MDPtools import Generate_random_MDP, sample_MDP_with_features_list
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 #################################################################
 
@@ -42,8 +43,8 @@ import numpy as np
 
 # Load Libraries
 
-from clustering import defaultNormal, UnifNormal, transformSamples, initializeClusters, splitter, Purity, plot_features
-
+from clustering import defaultNormal, UnifNormal, transformSamples, initializeClusters, splitter
+from testing import *
 #################################################################
 
 
@@ -54,7 +55,7 @@ from clustering import defaultNormal, UnifNormal, transformSamples, initializeCl
 
 # Set Parameters
 
-n = 50
+n = 30
 
 m = 3
 
@@ -64,11 +65,11 @@ deterministic = True
 
 pfeatures = 2
 
-sigma = [[0.01, 0], [0, 0.01]]
+sigma = [[0.05, 0], [0, 0.05]]
 
-N = 100000
+N = 1000
 
-T = 2
+T = 5
 
 clustering = ''
 
@@ -80,10 +81,10 @@ k = n_clusters
 
 classification = 'DecisionTreeClassifier'
 
-n_iter = 60
+n_iter = 26
 
 
-th = 20
+th = int(0.1*N*(T-1)/n)
 
 #################################################################
 
@@ -140,15 +141,15 @@ normal_distributions = UnifNormal(n,
                                      sigma)
 
 
-samples = sample_MDP_with_features_list(P,
-
-                                        R,
-
-                                        normal_distributions,
-
-                                        N,
-
-                                        T)
+#samples = sample_MDP_with_features_list(P,
+#
+#                                        R,
+#
+#                                        normal_distributions,
+#
+#                                        N,
+#
+#                                        T)
 
 #################################################################
 
@@ -160,10 +161,9 @@ samples = sample_MDP_with_features_list(P,
 
 # Transform into DataFrame
 
-df = transformSamples(samples,
-
-                      pfeatures)
-df0 = df.copy()
+#df = transformSamples(samples,
+#
+#                      pfeatures)
 #################################################################
 
 
@@ -174,15 +174,15 @@ df0 = df.copy()
 
 # Initialize Clusters
 
-df = initializeClusters(df,
-
-                        T,
-
-                        clustering=clustering,
-
-                        n_clusters=n_clusters,
-
-                        random_state=random_state)
+#df = initializeClusters(df,
+#
+#                        T,
+#
+#                        clustering=clustering,
+#
+#                        n_clusters=n_clusters,
+#
+#                        random_state=random_state)
 
 #################################################################
 
@@ -194,20 +194,53 @@ df = initializeClusters(df,
 
 # Run Iterative Learning Algorithm
 
-df_new = splitter(df,
-
-                  T,
-
-                  pfeatures,
-
-                  k,
-
-                  th,
-
-                  classification,
-
-                  n_iter)
+#df_new = splitter(df,
+#
+#                  T,
+#
+#                  pfeatures,
+#
+#                  k,
+#
+#                  th,
+#
+#                  classification,
+#
+#                  n_iter)
 
 #################################################################
-print(Purity(df_new))
-plot_features(df)
+#print(Purity(df_new))
+#plot_features(df)
+#
+#print('training error:',training_error(df_new))
+
+l = []
+samples = []
+for u in range(1,10):
+    N = 100*u
+    T = 5
+    th = int(0.1*N*(T-1)/n)
+    samples = samples + sample_MDP_with_features_list(P,
+                                        R,
+                                        normal_distributions,
+                                        N-int(N/10),
+                                        T)
+    df = transformSamples(samples,
+                      pfeatures)
+    df = initializeClusters(df,
+                        T,
+                        clustering=clustering,
+                        n_clusters=n_clusters,
+                        random_state=random_state)
+    df_new = splitter(df,
+                  T,
+                  pfeatures,
+                  k,
+                  th,
+                  classification,
+                  n_iter,
+                  OutputFlag = 0)
+    print('Iteration ', u,'Training accuracy ', training_accuracy(df_new)[0])
+    l.append(training_accuracy(df_new)[0])
+
+plt.plot(100*np.arange(1,10),l)
