cdd351e5fb8de737b38135eafc42516fb5b04ee0
Amine <amine_bennouna@hotmail.com>
2020-04-28 03:39:47 -0400

    Defined get_MDP + all error in value prediction function including R2

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """
+This file is intended in getting several test measure on the output of the algorithm
 Created on Sun Apr 26 23:13:09 2020
 
 @author: Amine
@@ -7,6 +8,7 @@ Created on Sun Apr 26 23:13:09 2020
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 #maps each OG_ClUSTER to a cluster --> We map a cluster to the OG_CLUSTER most 
 #present in it
@@ -31,6 +33,79 @@ def training_accuracy(df_new):
     accuracy_df = accuracy_df.groupby('OG_CLUSTER').mean()
     return (tr_accuracy,accuracy_df)
 
+#Get estimated MDP from clustering: for a given cluster s and action a, the next 
+# cluster is the one we go most to in the data when being in s and taking a
+def get_MDP(df_new):
+    #removing None values when counting where clusteres go
+    df0 = df_new[df_new['NEXT_CLUSTER']!='None']
+    transition_df = df0.groupby(['CLUSTER','ACTION','NEXT_CLUSTER'])['FEATURE_1'].count()
+    #taking the target cluster where we went the most
+    transition_df = transition_df.groupby(['CLUSTER','ACTION']).idxmax()
+    P_df = pd.DataFrame()
+    P_df['NEXT_CLUSTER'] = transition_df.apply(lambda x: x[2])
+    R_df = df_new.groupby('CLUSTER')['RISK'].mean()
+    return P_df,R_df
+
+#predicts value given a cluster and actions
+def predict_value_of_cluster(P_df,R_df,cluster,actions):
+    s = cluster
+    v = R_df.loc[s]
+    for a in actions:
+        s = P_df.loc[s,a].values[0]
+        v = v + R_df.loc[s]
+    return v
+
+#Compute E((\hat{v}-v)^2) ie the expect error in estimating the value given actions
+#NEED TO CHANGE: must adapt to case where T is diffrent from one simulation to another.
+# add Ids of simulations
+def traning_value_error(df_new,N,T):
+    E_v = 0
+    P_df,R_df = get_MDP(df_new)
+    for i in range(N):
+        s = df_new['CLUSTER'].loc[i*T]
+        a = df_new['ACTION'].loc[i*T]
+        v_true = df_new['RISK'].loc[i*T]
+        v_estim = R_df.loc[s]
+        for t in range(1,T):
+            v_true = v_true + df_new['RISK'].loc[i*T+t]
+            try:
+                s = P_df.loc[s,a].values[0]
+            # error raises in case we never saw a given transition in the data
+            except TypeError:
+                print('WARNING: Trying to predict next state from state',s,'taking action',a,', but this transition is never seen in the data. Data point:',i,t)
+            a = df_new['ACTION'].loc[i*T+t]
+            v_estim = v_estim + R_df.loc[s]
+        E_v = E_v + (v_true-v_estim)**2
+    return (E_v/N)
+#    return np.sqrt((E_v/N))/(R_df.max()*T)
+        
+#Computes the R square of value prediction
+def R2_value(df_new,N,T):
+    P_df,R_df = get_MDP(df_new)
+    E_v = 0
+    V_true = []
+    for i in range(N):
+        s = df_new['CLUSTER'].loc[i*T]
+        a = df_new['ACTION'].loc[i*T]
+        v_true = df_new['RISK'].loc[i*T]
+        V_true.append(v_true)
+        v_estim = R_df.loc[s]
+        for t in range(1,T):
+            v_true = v_true + df_new['RISK'].loc[i*T+t]
+            V_true.append(v_true)
+            try:
+                s = P_df.loc[s,a].values[0]
+            # error raises in case we never saw a given transition in the data
+            except TypeError:
+                print('WARNING: Trying to predict next state from state',s,'taking action',a,', but this transition is never seen in the data. Data point:',i,t)
+            a = df_new['ACTION'].loc[i*T+t]
+            v_estim = v_estim + R_df.loc[s]
+        E_v = E_v + (v_true-v_estim)**2
+    E_v = E_v/N
+    V_true = np.array(V_true)
+    v_mean = V_true.mean()
+    SS_tot = sum((V_true-v_mean)**2)/N
+    return 1- E_v/SS_tot
 # Returns the purity of each cluster
 def Purity(df):
     su = pd.DataFrame(df.groupby(['CLUSTER'])['OG_CLUSTER']
